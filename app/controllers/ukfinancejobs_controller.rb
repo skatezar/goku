c4567852a249cb0e90e366ebf2d7a4d054c0e1b9
laurenz gerold <skatezar@gmail.com>
2023-05-11 21:02:27 +0200

    ukfinancejob crud

--- app/controllers/ukfinancejobs_controller.rb
+++ app/controllers/ukfinancejobs_controller.rb
@@ -1,5 +1,50 @@
 class UkfinancejobsController < ApplicationController
+  before_action :set_ukfinancejob, only: [:show, :edit, :update, :destroy]
 
-	def index
-	end
+  def index
+    @ukfinancejobs = Ukfinancejob.all
+  end
+
+  def show
+  end
+
+  def new
+    @ukfinancejob = Ukfinancejob.new
+  end
+
+  def create
+    @ukfinancejob = Ukfinancejob.new(ukfinancejob_params)
+
+    if @ukfinancejob.save
+      redirect_to @ukfinancejob, notice: 'UK finance job was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @ukfinancejob.update(ukfinancejob_params)
+      redirect_to @ukfinancejob, notice: 'UK finance job was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ukfinancejob.destroy
+    redirect_to ukfinancejobs_url, notice: 'UK finance job was successfully destroyed.'
+  end
+
+  private
+
+  def set_ukfinancejob
+    @ukfinancejob = Ukfinancejob.find(params[:id])
+  end
+
+  def ukfinancejob_params
+    params.require(:ukfinancejob).permit(:title, :url, :type_of_job, :location, :deadline_date, :rolling_admission)
+  end
 end
