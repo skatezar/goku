65d7c7645fce3cb58a9fea2f9de5edd72bfc58c3
laurenz gerold <skatezar@gmail.com>
2023-05-29 20:33:20 +0200

    cleaned from chatgpt trial

--- app/helpers/chat_gpt_helper.rb
+++
@@ -1,2 +0,0 @@
-module ChatGptHelper
-end
--- test/controllers/chat_gpt_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ChatGptControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
