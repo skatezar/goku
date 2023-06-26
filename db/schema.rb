e95f7fe641942013cf1dfe99bb49f2eb23e39652
laurenz gerold <skatezar@gmail.com>
2023-06-26 22:51:00 +0200

    can create user account & styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_212942) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_203328) do
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
