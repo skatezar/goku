8a8c8e3fd10e09d427c0e7a1a4b2f9b20b362ee5
laurenz gerold <skatezar@gmail.com>
2023-06-26 22:32:41 +0200

    save before creating user

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,24 @@
 class BlogsController < ApplicationController
 	def index
+		@blogs = Blog.all
+	end
+
+	def new
+		@blog = Blog.new
+	end
+
+	def create
+		@blog = Blog.new(blog_params)
+		if @blog.save
+		    redirect_to root_path
+		else
+		    render :new
+		end
+	end
+
+	private
+
+	def blog_params
+		params.require(:mailform).permit(:title, :para1, :para2, :para3, :para4, :para5, :desc)
 	end
 end
--- app/controllers/coachingrequests_controller.rb
+++ app/controllers/coachingrequests_controller.rb
@@ -1,2 +1,25 @@
 class CoachingrequestsController < ApplicationController
+	def index
+		@coachingrequest = Coachingrequest.all
+	end
+
+	def new
+	    @coachingrequest = Coachingrequest.new
+    end
+
+	def create
+		@coachingrequest = Coachingrequest.new(coachingrequest_params)   
+	    if @coachingrequest.save
+		    redirect_to root_path
+		else
+		    render :new
+		    
+		end
+	end
+
+	private
+
+	def coachingrequest_params
+		params.require(:mailform).permit(:email, :description)
+	end
 end
