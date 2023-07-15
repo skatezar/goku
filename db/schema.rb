daadfc4614998452a6a4a72278f3a3ee199c1258
laurenz gerold <skatezar@gmail.com>
2023-07-15 11:52:52 +0200

    saving before creating user page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_192349) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_155130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_192349) do
     t.string "app_process"
   end
 
+  create_table "userprofiles", force: :cascade do |t|
+    t.text "name"
+    t.text "bio"
+    t.text "linkedin"
+    t.text "interest"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_userprofiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -110,4 +121,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_192349) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "userprofiles", "users"
 end
