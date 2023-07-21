4dee301b10f972e25044db81ec2efd99580c516a
laurenz gerold <skatezar@gmail.com>
2023-07-22 00:32:10 +0200

    make all fields mandatory

--- app/models/ukfinancejob.rb
+++ app/models/ukfinancejob.rb
@@ -1,6 +1,5 @@
 class Ukfinancejob < ApplicationRecord
 	has_one_attached :photo
 	has_many :comments, dependent: :destroy
-	validates :photo, presence: true
-
+	validates :title, :url, :company, :type_of_job, :location, :application_opening_date, :deadline_date, :eligibility, :industry, :hr_email, :app_process, :description, presence: true
 end
