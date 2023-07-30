659c378d8cefac6e5f5b69efd1e7096d9322b26e
laurenz gerold <skatezar@gmail.com>
2023-07-30 17:53:00 +0200

    mailer done though needs styling

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "contact@graduateflow.com"
   layout "mailer"
+
+  def default_url_options
+    { host: 'graduateflow.com' }
+  end
 end
