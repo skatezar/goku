11b149cd286df952cc8bae0699ece5aa3780d411
laurenz gerold <skatezar@gmail.com>
2023-08-04 11:58:08 +0200

    networking show done for mobile and validations for meetings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,8 +20,14 @@ class PagesController < ApplicationController
 
 	def networking
 		start_date = params.fetch(:start_date, Date.today).to_date
-		@meetings = Meeting.all
-		#start_date = params.fetch(:start_date, Date.today).to_date @meetings = Meeting.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+		@meetings = Meeting.order(:start_time)
+		@companies = Meeting.pluck(:company).uniq
+    	@start_times = Meeting.pluck(:start_time).uniq
+   		@industries = Meeting.pluck(:industry).uniq
+   		@locations = Meeting.pluck(:location).uniq
+	    @virtual_or_inperson_options = Meeting.pluck(:virtual_or_inperson).uniq
+
+    	filter_meetings if params[:company].present? || params[:start_time].present? || params[:industry].present?
 	end
 
 	def meetingfilter
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,7 @@
 class Meeting < ApplicationRecord
 	has_one_attached :photo1
 	has_many :meetingcomments
+	validates :name, :start_time, :photo1, :location, :virtual_or_inperson,:description, :link_to_url, :eligibility, :company, :industry, :deadline_date, presence: true
+
+
 end
